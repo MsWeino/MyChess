f386cf0dcb907ba25fe06d6a65982358a641603f
MsWeino <MsWeino@gmail.com>
2015-06-14 01:42:38 +0800

    ImageButton is OK

--- app/src/main/java/msweino/mychess/MainActivity.java
+++ app/src/main/java/msweino/mychess/MainActivity.java
@@ -2,61 +2,59 @@ package msweino.mychess;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.*;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 import msweino.mychess.Piece.Chessboard;
 
 public class MainActivity extends Activity {
-    final int[][] butXY_id={
-            {R.id.iButX0Y0,R.id.iButX1Y0,R.id.iButX2Y0,R.id.iButX3Y0,R.id.iButX4Y0,R.id.iButX5Y0,R.id.iButX6Y0,R.id.iButX7Y0,R.id.iButX8Y0},
-            {R.id.iButX0Y1,R.id.iButX1Y1,R.id.iButX2Y1,R.id.iButX3Y1,R.id.iButX4Y1,R.id.iButX5Y1,R.id.iButX6Y1,R.id.iButX7Y1,R.id.iButX8Y1},
-            {R.id.iButX0Y2,R.id.iButX1Y2,R.id.iButX2Y2,R.id.iButX3Y2,R.id.iButX4Y2,R.id.iButX5Y2,R.id.iButX6Y2,R.id.iButX7Y2,R.id.iButX8Y2},
-            {R.id.iButX0Y3,R.id.iButX1Y3,R.id.iButX2Y3,R.id.iButX3Y3,R.id.iButX4Y3,R.id.iButX5Y3,R.id.iButX6Y3,R.id.iButX7Y3,R.id.iButX8Y3},
-            {R.id.iButX0Y4,R.id.iButX1Y4,R.id.iButX2Y4,R.id.iButX3Y4,R.id.iButX4Y4,R.id.iButX5Y4,R.id.iButX6Y4,R.id.iButX7Y4,R.id.iButX8Y4},
-            {R.id.iButX0Y5,R.id.iButX1Y5,R.id.iButX2Y5,R.id.iButX3Y5,R.id.iButX4Y5,R.id.iButX5Y5,R.id.iButX6Y5,R.id.iButX7Y5,R.id.iButX8Y5},
-            {R.id.iButX0Y6,R.id.iButX1Y6,R.id.iButX2Y6,R.id.iButX3Y6,R.id.iButX4Y6,R.id.iButX5Y6,R.id.iButX6Y6,R.id.iButX7Y6,R.id.iButX8Y6},
-            {R.id.iButX0Y7,R.id.iButX1Y7,R.id.iButX2Y7,R.id.iButX3Y7,R.id.iButX4Y7,R.id.iButX5Y7,R.id.iButX6Y7,R.id.iButX7Y7,R.id.iButX8Y7},
-            {R.id.iButX0Y8,R.id.iButX1Y8,R.id.iButX2Y8,R.id.iButX3Y8,R.id.iButX4Y8,R.id.iButX5Y8,R.id.iButX6Y8,R.id.iButX7Y8,R.id.iButX8Y8},
-            {R.id.iButX0Y9,R.id.iButX1Y9,R.id.iButX2Y9,R.id.iButX3Y9,R.id.iButX4Y9,R.id.iButX5Y9,R.id.iButX6Y9,R.id.iButX7Y9,R.id.iButX8Y9}};
-    int tX=999,tY=999;
     Chessboard chessboard = new Chessboard();
+    int index = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        ImageButton iBuXY=(ImageButton)findViewById(R.id.iButX0Y0);
-//        TextView tVSystem=(TextView)findViewById(R.id.tVSystem);
+//
         findViews();
 
 
     }
 
-    void findViews(){
+    void findViews() {
+        //  ImageButton imageButton=(ImageButton) e.getSource();
+        ImageButton[] iBuXY = new ImageButton[90];
+        for (index = 0; index < 90; index++) {
+            iBuXY[index] = (ImageButton) findViewById(MyData.butXY_id[index]);
+            iBuXY[index].setOnClickListener(IBuXY_OC);
+        }
 
-        ImageButton[][] iBuXY=new ImageButton[9][10];
-        for (tX = 0 ;tX<5;tX++)
-            for (tY = 0; tY < 5; tY++) {
-
-                iBuXY[tX][tY] = (ImageButton) findViewById(butXY_id[tX][tY]);
-                iBuXY[tX][tY].setOnClickListener(new ImageButton.OnClickListener() {
-                    TextView tVSystem = (TextView) findViewById(R.id.tVSystem);
-
-                    // private int x=0,y=0;
-                    @Override
-                    public void onClick(View view) {
-                        tVSystem.setText("X =" +tX+"   Y="+tY);
-                        //Toast.makeText(MainActivity.this, TXT, Toast.LENGTH_SHORT)
-                          //      .show();
-                    }
-                });
-            }
     }
 
+    public View.OnClickListener IBuXY_OC =new View.OnClickListener(){
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+        TextView tVSystem = (TextView) findViewById(R.id.tVSystem);
+        for(int index=0;index<90;index++)
+        {
+            if(MyData.butXY_id[index]==v.getId())
+            {
+                String  TXT = "X = " +index%9+"Y = "+index/9;
+                tVSystem.setText(TXT);
+            }
+        }
+
+//        }
+
+    }
+
+};
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/msweino/mychess/MyData.java
@@ -0,0 +1,18 @@
+package msweino.mychess;
+
+/**
+ * Created by USER on 2015/6/13.
+ */
+public class MyData {
+    public final static int[] butXY_id={
+            R.id.iButX0Y0,R.id.iButX1Y0,R.id.iButX2Y0,R.id.iButX3Y0,R.id.iButX4Y0,R.id.iButX5Y0,R.id.iButX6Y0,R.id.iButX7Y0,R.id.iButX8Y0,
+            R.id.iButX0Y1,R.id.iButX1Y1,R.id.iButX2Y1,R.id.iButX3Y1,R.id.iButX4Y1,R.id.iButX5Y1,R.id.iButX6Y1,R.id.iButX7Y1,R.id.iButX8Y1,
+            R.id.iButX0Y2,R.id.iButX1Y2,R.id.iButX2Y2,R.id.iButX3Y2,R.id.iButX4Y2,R.id.iButX5Y2,R.id.iButX6Y2,R.id.iButX7Y2,R.id.iButX8Y2,
+            R.id.iButX0Y3,R.id.iButX1Y3,R.id.iButX2Y3,R.id.iButX3Y3,R.id.iButX4Y3,R.id.iButX5Y3,R.id.iButX6Y3,R.id.iButX7Y3,R.id.iButX8Y3,
+            R.id.iButX0Y4,R.id.iButX1Y4,R.id.iButX2Y4,R.id.iButX3Y4,R.id.iButX4Y4,R.id.iButX5Y4,R.id.iButX6Y4,R.id.iButX7Y4,R.id.iButX8Y4,
+            R.id.iButX0Y5,R.id.iButX1Y5,R.id.iButX2Y5,R.id.iButX3Y5,R.id.iButX4Y5,R.id.iButX5Y5,R.id.iButX6Y5,R.id.iButX7Y5,R.id.iButX8Y5,
+            R.id.iButX0Y6,R.id.iButX1Y6,R.id.iButX2Y6,R.id.iButX3Y6,R.id.iButX4Y6,R.id.iButX5Y6,R.id.iButX6Y6,R.id.iButX7Y6,R.id.iButX8Y6,
+            R.id.iButX0Y7,R.id.iButX1Y7,R.id.iButX2Y7,R.id.iButX3Y7,R.id.iButX4Y7,R.id.iButX5Y7,R.id.iButX6Y7,R.id.iButX7Y7,R.id.iButX8Y7,
+            R.id.iButX0Y8,R.id.iButX1Y8,R.id.iButX2Y8,R.id.iButX3Y8,R.id.iButX4Y8,R.id.iButX5Y8,R.id.iButX6Y8,R.id.iButX7Y8,R.id.iButX8Y8,
+            R.id.iButX0Y9,R.id.iButX1Y9,R.id.iButX2Y9,R.id.iButX3Y9,R.id.iButX4Y9,R.id.iButX5Y9,R.id.iButX6Y9,R.id.iButX7Y9,R.id.iButX8Y9};
+}
